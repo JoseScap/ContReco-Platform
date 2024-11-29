5b2d060b6096f82a45fa2c850d0b3bf83cf01d85
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-11-29 02:51:33 -0300

    Init CMS

---
+++ apps/ContentManagementService/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    [HttpGet("[action]")]
+    public string Ping() => "Pong";
+}
