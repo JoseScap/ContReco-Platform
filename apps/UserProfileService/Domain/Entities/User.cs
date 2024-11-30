8d4d3526cc974fe7916ce00ee6ef70894f4904da
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-11-30 17:54:21 -0300

    Add UpsContext and User entity

---
+++ apps/UserProfileService/Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace UPS.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateTime Birthday { get; set; }
+    public DateTime CreatedDate { get; set;}
+    public DateTime ModifiedDate { get; set; }
+}
