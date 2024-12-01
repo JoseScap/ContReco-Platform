e647f777db0c711dd614ab0be86bb90eb86657bc
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-12-01 15:06:30 -0300

    Add constructors to 'Base' and 'User' entities

--- libs/SharedKernel/Domain/Entities/BaseEntity.cs
+++ libs/SharedKernel/Domain/Entities/BaseEntity.cs
@@ -5,4 +5,15 @@ public abstract class BaseEntity
     public Guid Id { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime ModifiedDate { get; set; }
+
+    public BaseEntity()
+    {
+    }
+
+    public BaseEntity(Guid id, DateTime? createdDate, DateTime? modifiedDate)
+    {
+        Id = id;
+        if (createdDate is not null) CreatedDate = createdDate.Value;
+        if (modifiedDate is not null) ModifiedDate = modifiedDate.Value;
+    }
 }
--- libs/SharedKernel/Domain/Entities/User.cs
+++ libs/SharedKernel/Domain/Entities/User.cs
@@ -8,4 +8,19 @@ public class User : BaseEntity
     public string Email { get; set; }
     public string Password { get; set; }
     public DateTime Birthday { get; set; }
+
+    public User()
+    {
+    }
+
+    public User(Guid id, string firstName, string lastName, string userName, string email, string password, DateTime birthday, DateTime? createdDate, DateTime? modifiedDate)
+        : base (id, createdDate, modifiedDate)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        UserName = userName;
+        Email = email;
+        Password = password;
+        Birthday = birthday;
+    }
 }
