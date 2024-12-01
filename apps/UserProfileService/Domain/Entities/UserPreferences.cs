5cbca0815a35250b0fd75ae78eec9f0c2d2b698b
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-12-01 12:20:38 -0300

    Add UserPreferences

---
+++ apps/UserProfileService/Domain/Entities/UserPreferences.cs
@@ -0,0 +1,10 @@
+﻿using Libs.SK.Domain.Entities;
+
+namespace UPS.Domain.Entities;
+
+public class UserPreferences : BaseEntity
+{
+    public List<string> Tags { get; set; }
+    public Guid UserId { get; set; }
+    public virtual User User { get; set; }
+}
