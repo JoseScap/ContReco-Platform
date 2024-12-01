e30a121cb81acbf17b77573652cb1ee12e22e788
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-12-01 12:19:38 -0300

    Add BaseEntity

---
+++ libs/SharedKernel/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Libs.SK.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
